79e0d5c42905ba65fbd3c829b44dc65e9905b086
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2024-05-23 14:53:25 +0800

    fetch

--- fetch.sh
+++ fetch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for url in $(lb links https://travel.state.gov/content/travel/en/legal/visa-law0/visa-statistics/immigrant-visa-statistics/monthly-immigrant-visa-issuances.html --text-include 'Issuances by FSC' --path-include .pdf);
+lb links https://travel.state.gov/content/travel/en/legal/visa-law0/visa-statistics/immigrant-visa-statistics/monthly-immigrant-visa-issuances.html --text-include 'Issuances by FSC' --path-include .pdf | while read -r url;
 do
     date=$(lb dates "$url")
     file="data/${date}.jsonl"
