6874713f53a1b58d89932f3a9bb3297888e1f675
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2024-05-23 17:49:35 +0800

    fetch requirements

--- fetch.fish
+++ fetch.fish
@@ -6,6 +6,7 @@ for url in (lb links https://travel.state.gov/content/travel/en/legal/visa-law0/
 
     if not test -e "$file"
         echo "Saving $file"
+        echo "$url"
         lb markdown-tables "$url" --start-row 1 --concat --to-json > "$file"
     end
 end
